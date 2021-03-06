8f4c05711549cdf9d03464fe479ca3169d29e1b4
CongelateurLeBg <79990658+CongelateurLeBG@users.noreply.github.com>
2021-03-06 09:16:25 +0000

    Add files via upload

---
+++ NitroGen.py
@@ -0,0 +1,18 @@
+import random, string
+
+print("""
+███╗   ██╗██╗████████╗██████╗  ██████╗      ██████╗ ███████╗███╗   ██╗
+████╗  ██║██║╚══██╔══╝██╔══██╗██╔═══██╗    ██╔════╝ ██╔════╝████╗  ██║
+██╔██╗ ██║██║   ██║   ██████╔╝██║   ██║    ██║  ███╗█████╗  ██╔██╗ ██║
+██║╚██╗██║██║   ██║   ██╔══██╗██║   ██║    ██║   ██║██╔══╝  ██║╚██╗██║
+██║ ╚████║██║   ██║   ██║  ██║╚██████╔╝    ╚██████╔╝███████╗██║ ╚████║
+╚═╝  ╚═══╝╚═╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝      ╚═════╝ ╚══════╝╚═╝  ╚═══╝""")
+amount = int(input('Enter how many Nitro you want :'))
+value = 1
+while value <= amount:
+    code = "https://discord.gift/" + ('').join(random.choices(string.ascii_letters + string.digits, k=16))
+    f = open('Codes.txt', "a+")
+    f.write(f'{code}\n')
+    f.close()
+    print(f'[Code Nitro:] {code}')
+    value += 1
